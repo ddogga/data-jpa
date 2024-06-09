ff7bbcf39102da686d41342bc7f6b5fcc3008d14
ddogga <hayeonlee60327@gmail.com>
2024-06-09 17:37:43 +0900

    스프링 데이터 JPA와 DB설정, 동작확인

---
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package study.data_jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.data_jpa.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+
+
+}
